ff366f50e9b6e1372b37a126ba5dc16f47efcde9
Shrikant Vishwakarma <64365738+StrixV54@users.noreply.github.com>
2025-05-13 17:03:58 +0530

    sds

--- next.config.js
+++ next.config.js
@@ -3,6 +3,5 @@
 module.exports = {
   images: {
     domains: ["static.cx.metamask.io", "placehold.co", "assets.coingecko.com", "tokens.pancakeswap.finance", "cdn.moralis.io",  "logo.moralis.io", "imageplaceholder.net", "raw.githubusercontent.com", "lcw.nyc3.cdn.digitaloceanspaces.com", "stellar.myfilebase.com"],
-  },
-  output: 'export', // Add this for static export
+  }
 };
